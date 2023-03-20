f32f766c4d5e7958e1ff30a267123a0d314e3e55
Denis Korneev <38877578+odeszaxoxo@users.noreply.github.com>
2023-03-20 17:45:49 +0400

    Change ports

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 
-const port = 3001;
+const port = process.env.PORT || 10000;
 
 const app = express();
 const cors = require('cors');
